72f4f00329418cbf856ade715561e643eeb4e17d
Gaurav Sharma <98760746+IsmartGaurav@users.noreply.github.com>
2024-08-04 18:43:07 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const YOUTUBE_API_KEY = "AIzaSyCGm1DHejMPG4bJVO0a7AJO4Ks0ipFPLFo"
+const YOUTUBE_API_KEY = ""
 export const YOUTUBE_VID_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=48&hl=pa&relevanceLanguage=pa&regionCode=IN&videoCategoryId=10&key=" + YOUTUBE_API_KEY;
 
 export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q="
