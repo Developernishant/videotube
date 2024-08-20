51c103888c808489541e868847dae1131fefa696
Gaurav Sharma <98760746+IsmartGaurav@users.noreply.github.com>
2024-08-20 17:21:31 +0530

    Create CommentContainer.js

---
+++ src/components/CommentContainer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import CommentList from "./CommentList";
+
+const CommentContainer = ( comments ) => {
+   
+  return <CommentList data={comments} />
+};
+
+export default CommentContainer;
