0cbc0105686e3fd0e0b189d4ba5da500a38f0a27
Gaurav Sharma <98760746+IsmartGaurav@users.noreply.github.com>
2024-08-20 17:21:09 +0530

    Create CommentList.js

---
+++ src/components/CommentList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Comment from "./Comment";
+
+const CommentList = ({ data }) => {
+    if (!data?.data?.items || data.data.items.length === 0) {
+        return null;
+    }
+    return (
+        <div className="w-4/6">
+        <div className="font-bold text-xl ml-12 mt-5">
+            <h1>Comments</h1>
+        </div>
+            {data.data.items.map((item, index) => (
+                <Comment key={index} comment={item} />
+            ))}
+        </div>
+    );
+};
+
+export default CommentList;
