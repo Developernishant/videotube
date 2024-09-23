c4d21c4376dde7b499361a9dd978c8accadbeef7
IsmartGaurav <freeguy5530@gmail.com>
2024-09-23 07:28:36 +0530

    fixed some minor bug

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -3,7 +3,7 @@ import React from "react";
 const Comment = ({ comment }) => {
     const { textOriginal, authorDisplayName, authorProfileImageUrl } = comment.snippet.topLevelComment.snippet;
     return (
-        <div className="mt-3 flex p-1 pl-3 bg-slate-50 rounded-lg w-full">
+        <div className="mt-3 flex p-1 pl-3 bg-slate-200 rounded-lg w-full">
             <img
                 className="h-8 w-8 mt-1 rounded-full"
                 alt="user"
